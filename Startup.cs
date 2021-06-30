f7a12d69e6e4f509e7c596c47e67af448759d9d1
Felipe C <fcabeza001@gmail.com>
2021-06-30 11:33:04 -0500

    crud operations done

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -14,7 +15,7 @@ using Microsoft.OpenApi.Models;
 
 namespace Catalog
 {
-    public class Startup
+    public class Startup 
     {
         public Startup(IConfiguration configuration)
         {
@@ -26,6 +27,7 @@ namespace Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IInMemItems, InMemItems>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
